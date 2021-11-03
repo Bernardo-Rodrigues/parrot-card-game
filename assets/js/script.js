cee10f21afd0ea50dcf92e51ac4456dd35fc0e5b
Bernardo Rodrigues <bernardo.rodrigues06@unifei.edu.br>
2021-11-03 16:10:07 -0300

    Layout pronto

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+function jogar (cartas) {
+    let telaStart = document.querySelector(".start");
+    let telaGame = document.querySelector(".game");
+    telaStart.classList.add("none");
+    telaGame.classList.remove("none");
+    telaGame.classList.add("flex");
+}
